21cf04bc14b9951778d012cf140db272c46a4d05
3119005430 <1026762224@qq.com>
2021-10-26 19:55:07 +0800

    项目完成！ 修复了昨天没看到的小bug

--- FourArithmetic/src/pairing/Main.java
+++ FourArithmetic/src/pairing/Main.java
@@ -6,72 +6,6 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    /*    public static void main(String[] args) throws Exception {
-            File file1 = new File("UserAnswers.txt");
-            File file2 = new File("Answers.txt");
-            File file3 = new File("Exercises.txt");
-            System.out.println("---------四则运算表达式--------");
-            System.out.println("      -n :生成题目的个数       ");
-            System.out.println("      -r :题目数值范围         ");
-            System.out.println("      -g :查看测试结果         ");
-            System.out.println("      -go:执行               ");
-            System.out.println("      -exit:退出             ");
-            System.out.println("请输入你的指令:");
-            Scanner op = new Scanner(System.in);
-            int n = 1;
-            int r = 1;
-            try {
-                while (op.hasNext()) {
-                    switch (op.next()) {
-                        case "-n" -> {
-                            System.out.println("请输入你要生成的题目个数:");//-n 生成练习的数目，循环调用generation对象
-                            try {
-                                n = op.nextInt();
-                            } catch (InputMismatchException ignored) {
-                                System.out.println("非法输入!");
-                            }
-                        }
-                        case "-r" -> {
-                            System.out.println("请输入生成题目的数值范围:");//-r 传入参数，控制数值范围
-                            try {
-                                r = op.nextInt();
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        case "-g" -> {
-                            GradeCheck gradeCheck = new GradeCheck();//答案和做题文档对比，调用ResultCheck对象 生成grade.txt文件                   break;
-                            gradeCheck.GetGrade(file1, file2);
-                        }
-                        case "-go" -> {
-                            for (int i = 0; i < n; i++) {
-                                Generation g = new Generation();
-                                String ques = g.GenerateExp(r);//随机生成表达式
-
-                                Calculation c = new Calculation();
-                                String ans = c.cal(c.toHouzhui(ques));//计算表达式结果
-
-                                if (ans.contains("-")) { //处理结果是负数情况
-                                    i = i - 1;
-                                    continue;
-                                }
-                                QuestionSaving qs = new QuestionSaving();
-                                qs.Write(ques);//将表达式写入文档
-                                AnswerSaving as = new AnswerSaving();
-                                as.Write(ans);//将结果写入答案文档
-                            }
-                            new QuestionSaving().Saving(file3);//将问题追加保存到Exercises.txt文件中
-                            new AnswerSaving().Saving(file2);//将答案追加保存到Answer.txt文件中
-                        }
-                        case "-exit" -> System.exit(0);
-                        default -> System.out.println("输入指令错误，请重新输入");
-                    }
-                    System.out.println("请输入你的指令:");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }*/
     private static boolean setRange = false;
     private static boolean setNumber = false;
     private static boolean setExerciseFile = false;
@@ -79,7 +13,7 @@ public class Main {
     private static String exerciseFile = "UserAnswers.txt";
     private static String answerFile = "Answers.txt";
     private static int number;
-    private static int range=2 ;
+    private static int range = 2;
 
     public static void main(String[] args) {
         File file1 = new File("UserAnswers.txt");
@@ -113,9 +47,8 @@ public class Main {
                     Main.setAnswerFile = true;
                 }
             }
-            if (!setRange) {
-                System.out.println("请输入正确参数！");
-            } else {
+            if (setNumber && setRange) {
+//                System.out.println("请输入正确参数！");
                 for (int i = 0; i < Main.number; i++) {
                     Generation g = new Generation();
                     String ques = g.GenerateExp(Main.range);//随机生成表达式
@@ -134,19 +67,18 @@ public class Main {
                 }
                 new QuestionSaving().Saving(file3);//将问题追加保存到Exercises.txt文件中
                 new AnswerSaving().Saving(file2);//将答案追加保存到Answer.txt文件中
-            }
-
-            if (!Main.setExerciseFile && !Main.setAnswerFile) {
-
-            } else if (Main.setExerciseFile && Main.setAnswerFile) {
+            } else if (setExerciseFile && setAnswerFile) {
                 GradeCheck gradeCheck = new GradeCheck();//答案和做题文档对比，调用ResultCheck对象 生成grade.txt文件                   break;
                 gradeCheck.GetGrade(file1, file2);
+            } else {
+                System.out.println("请输入正确参数！");
             }
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("");
+            e.printStackTrace();
+            System.out.println("数组越界异常！");
         } catch (NumberFormatException e) {
-             e.printStackTrace();
-            System.out.println("");
+            e.printStackTrace();
+            System.out.println("数据类型错误！");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -155,6 +87,7 @@ public class Main {
     private static void setRange(int range) {
         Main.range = range;
     }
+
     private static void setNumber(int num) {
         Main.number = num;
     }
